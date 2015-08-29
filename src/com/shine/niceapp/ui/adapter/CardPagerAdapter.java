d281374752de6db580d3386bb5974d0c15fa7133
Coder_xing <dreamerzhx@163.com>
2015-08-29 11:01:57 +0800

    first commit for eclipse

---
+++ src/com/shine/niceapp/ui/adapter/CardPagerAdapter.java
@@ -0,0 +1,72 @@
+package com.shine.niceapp.ui.adapter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.shine.niceapp.bean.Card;
+import com.shine.niceapp.ui.fragment.CardFragment;
+
+public class CardPagerAdapter extends FragmentStatePagerAdapter {
+	private List<Card> mPostList;
+	private List<Fragment> mFragments = new ArrayList();
+
+	public CardPagerAdapter(FragmentManager paramFragmentManager,
+			List<Card> paramList) {
+		super(paramFragmentManager);
+		Iterator localIterator = paramList.iterator();
+		while (localIterator.hasNext()) {
+			Card localAppModel = (Card) localIterator.next();
+			this.mFragments.add(CardFragment.getInstance(localAppModel));
+		}
+		this.mPostList = paramList;
+	}
+
+	public void addCardList(List<Card> cardList) {
+		ArrayList localArrayList = new ArrayList();
+		Iterator localIterator = cardList.iterator();
+		while (localIterator.hasNext())
+			localArrayList.add(CardFragment.getInstance((Card) localIterator
+					.next()));
+		if (this.mFragments == null)
+			this.mFragments = new ArrayList();
+		this.mFragments.addAll(localArrayList);
+		this.mPostList.addAll(cardList);
+	}
+
+	public List<Card> getCardList() {
+		return this.mPostList;
+	}
+
+	@Override
+	public int getCount() {
+		return this.mFragments.size();
+	}
+
+	public List<Fragment> getFragments() {
+		return this.mFragments;
+	}
+
+	@Override
+	public Fragment getItem(int paramInt) {
+		return this.mFragments.get(paramInt);
+	}
+
+	public void setCardList(List<Card> cardList) {
+		ArrayList localArrayList = new ArrayList();
+		Iterator localIterator = cardList.iterator();
+		while (localIterator.hasNext())
+			localArrayList.add(CardFragment.getInstance((Card) localIterator
+					.next()));
+		this.mFragments = localArrayList;
+		this.mPostList = cardList;
+	}
+
+	public void setFragments(List<Fragment> paramList) {
+		this.mFragments = paramList;
+	}
+}
